c4a58e62194761102b04abd89b0294d11498fbf0
sujansijakhow <sujansijakhow@gmail.com>
2025-01-20 19:42:05 +0545

    SideMenu feature added

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -6,8 +6,10 @@ const SideMenu = () => {
             <span className="text-[70px] pb-3 font-bold text-slate-800">ATS</span>
             <div className="flex flex-col items-start w-full text-xl font-semibold text-slate-700 mt-4 gap-2">
                 <SideMenuHomeButton />
+                <div className="w-full p-2 cursor-pointer hover:bg-gray-100 flex flex-row items-center justify-between">Settings</div>
+                <div className="w-full p-2 cursor-pointer hover:bg-gray-100 flex flex-row items-center justify-between">About</div>
+                <div className="w-full p-2 cursor-pointer hover:bg-gray-100 flex flex-row items-center justify-between">Contact us</div>
             </div>
-            <div></div>
         </div>
     )
 }
