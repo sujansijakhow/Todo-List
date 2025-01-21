62c2942662aef84ad35a7ad62c03e94a2fde5b54
sujansijakhow <sujansijakhow@gmail.com>
2025-01-21 13:42:09 +0545

    Context feature added

---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+import TodoComponent from "./TodoList/TodoComponent";
+
+const Content = () => {
+    retunr (
+        <div className="w-5/6 bg-gray-50 flex flex-row justify-center items-center">
+            <TodoComponent />
+
+        </div>
+    )
+}
