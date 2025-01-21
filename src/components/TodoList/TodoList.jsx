fb1a17c4c2f1e31b64f9c41250073598424559c8
sujansijakhow <sujansijakhow@gmail.com>
2025-01-21 13:31:13 +0545

    TodoList feature added

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,14 @@
+import TodoListItem from "./TodoListItem";
+
+const TodoList = ({ todoList }) => {
+    return (
+        <div className="divide-y mt-4">
+            {
+                todoList.map(todo => <TodoListItem todo = {todo}/>)
+            }
+
+        </div>
+    )
+}
+
+export default TodoListItem;
